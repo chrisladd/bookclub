e322f8c1b0beae3982ae8982fd4ca9a4fbad9a6d
Chris Ladd <christopher.ladd@nytimes.com>
2014-02-27 14:49:03 -0500

    adds code from Thursday book club

--- week-2/Demo2/main.m
+++ week-2/Demo2/main.m
@@ -13,142 +13,170 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        
 #pragma mark - Numbers
-        // float, CGFloat
-        // int, NSInteger
-        // long, long long, unsigned, signed
-        
-        // id.
-        
-        
-        // +-*/, (), %
-        
-        // int i = (int)f;
-        
-        // +=, -=, *=, /=
-        
-        
-        
+
+        int i = 0;
+        i = i + 5;
+
+        NSLog(@"The value of i is %d. You could also do this: %i", i, i);
+
+        float f = 0.2;
+        double d = 0.2;
+
+        NSLog(@"Print a float: %f. and a double: %f", f, d);
+
+        NSInteger integer = 0;
+        CGFloat floatThing = 0;
+
+        NSLog(@"integers are just integers! %ld, %f", (long)integer, floatThing);
+
+        NSUInteger unsigendInt = -1;
+
+        NSLog(@"integer: %lu", unsigendInt);
+
+        NSString *string = [NSString stringWithFormat:@"I am a string with a float: %.2f", floatThing];
+
+        NSLog(@"%@", string);
+
+        i = i + 1;
+        i++;
+        i += 1;
+
+
+        i--;
+        i -= 1;
+        i = i - 1;
+
+        i = i * 5;
+        i *= 5;
+
+        i = i / 5;
+        i /= 5;
+
+
+        // mod
+        i = 48;
+        int remainder = i % 5;
+        NSLog(@"remainder: %d", remainder);
+
+
+
         
 #pragma mark - Control Statements
-        
-        // for
-        // <, >, ==, <=, >=
-        
-        
-        
-        // scanf
-        
-        // introduce NSArray. introduce fast enumeration
-        // nested loops. (for, for)
-        // while loops.
-        
-        int i = 0;
+
+
+        for (int i = 0; i < 10; i++) {
+            NSLog(@"I am in a loop: %d", i);
+        }
+
+        i = 0;
+        while (i < 10) {
+            NSLog(@"WHILE I am in a loop: %d", i);
+            i++;
+        }
+
+
+        i = 0;
         do {
+            NSLog(@"DO WHILE I am in a loop %d", i);
             i++;
         } while (i < 10);
-        
-        
+
+
+        i = 0;
         while (YES) {
+            NSLog(@"DO WHILE I am in a loop %d", i);
+
             i++;
-            
-            if (i < 10) {
+            if (i >= 10) {
                 break;
             }
         }
-        
-        // print out all odd numbers up to 100
-        while (YES) {
-            i++;
-            
-            if (i % 2 == 0) {
-                continue;
+
+
+        // for objects
+
+        NSArray *anArrayOfPeopleNames = @[@"Shirwah", @"Adam", @"Grant", @"Agnes"];
+
+        NSArray *array = [[NSArray alloc] initWithObjects:@"some", @"objects", nil];
+
+        NSLog(@"%@", array);
+
+        NSArray *anotherWay = [NSArray arrayWithObjects:@"other", @"objects", nil];
+
+        NSLog(@"another array: %@", anotherWay);
+
+        for (int i = 0; i < [anArrayOfPeopleNames count]; i++) {
+            NSString *name = [anArrayOfPeopleNames objectAtIndex:i];
+
+            NSLog(@"name: %@", name);
+        }
+
+        for (NSString *name in anArrayOfPeopleNames) {
+            NSLog(@"fastEnumerationName: %@", name);
+        }
+
+
+        // HOW DO YOU KNOW WHAT'S IN THE ARRAY?
+        // id signifies an object of an unknown type.
+        NSArray *arrayOfStringsAndNumbers = @[@"A string",
+                                              @(4),
+                                              [NSNumber numberWithInteger:7],
+                                              [NSString stringWithFormat:@"another string"]];
+
+
+        NSInteger runningTotalOfArray = 0;
+        for (id anObject in arrayOfStringsAndNumbers) {
+            if ([anObject isKindOfClass:[NSString class]]) {
+                // it's a string
+                NSString *string = (NSString *)anObject;
+
+                NSLog(@"enumate a string: %@ (%ld)", string, [string length]);
+//                NSLog(@"this should crash: %ld", [string count]);
             }
-            
-            NSLog(@"%d", i);
-            
-            if (i >= 100) {
-                break;
-            }
-        }
-        
-        
-        // if
-        // else
-        // else if
-        
-        
-        if (i == 2) {
-            
-        }
-        else if (i == 6) {
-            
-        }
-        else {
-            
-        }
-        
-        
-        // switch
-        
-        switch (i) {
-            case 2:
-                NSLog(@"it's two");
-                break;
-            case 6:
-                NSLog(@"it's four");
-            case 8:
-                NSLog(@"it's eight! Or six!");
-                break;
-            default:
-                NSLog(@"it's not any!");
-                break;
-        }
-        
-        
-        
-        // && ||
-        
-        if (i == 6 || i == 8) {
-            NSLog(@"it's eight! or six!");
-        }
-        
-        if (i > 10 && i < 20) {
-            
-        }
-        
-        if (i > 10) {
-            if (i < 20) {
-                NSLog(@"");
+            else if ([anObject isKindOfClass:[NSNumber class]]) {
+                // it's a number
+                runningTotalOfArray += [(NSNumber *)anObject integerValue];
             }
         }
-        
-        
-        // BOOL
-        
-        BOOL aBool = YES;
-        BOOL isBig = i > 100;
-        
-        // ternary
-        BOOL isTernaryBig = i > 100 ? YES : NO;
-        
-        
-        
-#pragma mark - More on Classes
-        // separate .h, .m
-        //        @property
-        // multiple arguments
-        // argument names
-        // static (yikes!)
-        
-        static int GlobalInt = 0;
-        
-        // inside a function, scope is different.
-        
-        // self. 
-        
-    
+
+        NSLog(@"Total Value: %ld", runningTotalOfArray);
+
+
+#pragma mark Casting
+        NSString *aCastableString = @"a string";
+//        NSUInteger length = [aCastableString length];
+//
+//        NSUInteger count = [(NSArray *)aCastableString count];
+//
+//        NSLog(@"%ld, %ld", length, count);
+
+        NSString *desc = [aCastableString description];
+
+        NSString *desc2 = [(NSArray *)aCastableString description];
+
+        NSLog(@"%@, %@", desc, desc2);
+
+
+
+#pragma mark - Misusing methods
+
+        NSArray *myArray = @[@"yet", @"another", @"array"];
+        NSArray *anotherArray = @[@"more", @"things"];
+
+        NSLog(@"%@", myArray);
+
+        NSArray *combinedArray = [myArray arrayByAddingObjectsFromArray:anotherArray];
+
+        NSLog(@"%@", combinedArray);
+
+
+        // the wrong way
+        NSString *notAnArray = @"notAnArray";
+        NSArray *crashableArray = [myArray arrayByAddingObjectsFromArray:(NSArray *)notAnArray];
+
+        NSLog(@"%@", crashableArray);
+
     }
     return 0;
 }
